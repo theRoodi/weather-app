b1683155b5476a9bd1797d884e9a574a2093305a
roodi <igorrud95@gmail.com>
2022-01-27 10:20:10 +0700

    done app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const search = (evt) => {
     if (evt.key === 'Enter') {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json)
+        .then(res => res.json())
         .then(result => {
           setWeather(result)
           setQuery('')
@@ -35,26 +35,33 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={
+      (typeof weather.main != 'undefined')
+        ? ((weather.main.temp > 10)
+          ? 'app warm': 'app')
+        :'app'}>
       <main>
         <div className='search-box'>
           <input
             type='text'
             className='search-bar'
             placeholder='Search...'
-            onChange={e => setQuery(e.target.value)} 
+            onChange={e => setQuery(e.target.value)}
             value={query}
-            onKeyPress={search}/>
+            onKeyPress={search} />
         </div>
-        <div className='location-box'>
-          <div className='location'>Novokuznetsk, RU</div>
-          <div className='date'>{dateBuilder(new Date())}</div>
+        {(typeof weather.main != 'undefined') ? (
+          <div>
+          <div className='location-box'>
+            <div className='location'>{weather.name}, {weather.sys.country}</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
+          <div className='weather-box'>
+            <div className='temp'>{Math.round(weather.main.temp)}°C</div>
+            <div className='weather'>{weather.weather[0].main}</div>
+          </div>
         </div>
-        <div className='weather-box'>
-          <div className='temp'>-15°C</div>
-          <div className='weather'>Coldy</div>
-
-        </div>
+        ) : ('')}
       </main>
     </div>
   );
